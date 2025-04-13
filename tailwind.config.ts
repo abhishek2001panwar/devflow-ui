8da3c3d428d4505f3e671c3c59e8c9b0b2726b47
Abhishekpanwar10 <abhishek2001panwar@gmail.com>
2025-04-13 14:57:46 +0530

    fix: update main and types paths in package.json; adjust Tailwind CSS content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,9 @@
-import path from "path";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         './src/**/*.{js,ts,jsx,tsx}',
-        path.join(__dirname, '../devflow-ui/src/lib/ui/**/*.{js,ts,jsx,tsx}'), // use path.join for safety
+        './node_modules/devflow-ui/**/*.{js,ts,jsx,tsx}', // use path.join for safety
       ],
     theme: {
       extend: {},

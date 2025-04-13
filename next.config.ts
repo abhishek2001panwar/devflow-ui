63a373fb260456e60befbf66b17b382e0524fe53
Abhishekpanwar10 <abhishek2001panwar@gmail.com>
2025-04-13 14:22:27 +0530

    fix: update configuration files for consistency and clarity

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+ 
+  assetPrefix: "",
+  trailingSlash: true,
 };
 
 export default nextConfig;
